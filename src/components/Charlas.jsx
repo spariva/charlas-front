7cf46a92118558934702482214985569e50aedab
Sofía <sofiavillarejo.dev@gmail.com>
2024-12-19 00:57:43 +0100

    Revert "avances"
    This reverts commit 087fb01d4b6fd24dbc8805597c15e6504a860bbb.

--- src/components/Charlas.jsx
+++ src/components/Charlas.jsx
@@ -4,11 +4,10 @@ import Card from "./CardCharla";
 
 class Charlas extends Component {
   state = {
-    charlas: [],
-    rondas: [],
-    charlasRonda: []
+    charlas: []
   }
-  getCharlas = () => {
+
+  componentDidMount = () => {
     services.getCharlas().then((response) => {
       console.log(response);
       this.setState({
@@ -17,38 +16,14 @@ class Charlas extends Component {
     });
   }
 
-  getRondas = () => {
-    services.getRondas().then((response) => {
-      console.log(response);
-      this.setState({
-        rondas: response
-      });
-    });
-  }
-  // getCharlasRonda = () => {
-  //   services.getCharlasRonda().then((response) => {
-  //     console.log(response);
-  //     this.setState({
-  //       charlasRonda: response
-  //     });
-  //   })
-  // }
-
-  componentDidMount = () => {
-    this.getCharlas();
-    this.getRondas();
-  }
-
   render() {
     return (
       <div className="container">
         <h1 className="my-4 text-center">Charlas</h1>
-
-        {/* Fila de charlas */}
-        <div className="row d-flex flex-wrap justify-content-start">
+        <div className="row">
           {this.state.charlas.map((charla, index) => {
             return (
-              <div key={index} className="col-12 col-sm-6 col-md-4 mb-4">
+              <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                 <Card
                   imagen={charla.imagenCharla}
                   titulo={charla.titulo}
@@ -58,27 +33,8 @@ class Charlas extends Component {
             );
           })}
         </div>
-
-        <div className="row d-flex justify-content-end mt-4">
-          <div className="col-12 col-md-3">
-            <select
-              value={this.state.selectedRonda}
-              onChange={this.handleRondaChange}
-              className="form-select"
-            >
-              <option value="">Selecciona Ronda</option>
-              {this.state.rondas.map((ronda, index) => {
-                return (
-                  <option key={index} value={ronda.idRonda}>
-                    Ronda {ronda.idRonda}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-        </div>
       </div>
-    )
+    );
   }
 }
 

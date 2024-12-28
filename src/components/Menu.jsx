fcec26c1184f6717a8ad015a871777007d39ab89
Oscargm13 <oskitargm5@gmail.com>
2024-12-28 13:37:50 +0100

    createRonda

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../assets/css/menu.css'
 import { NavLink } from 'react-router-dom'
+import services from '../services/services'
 
 export default class Menu extends Component {
   render() {
@@ -32,12 +33,18 @@ export default class Menu extends Component {
 					<h4>Charlas</h4>
 				</div>
 			</NavLink>
-			<a href="#">
+			<NavLink to="/createronda">
+			<div className="option">
+					<i className="far fa-sticky-note"></i>
+					<h4>Crear Ronda</h4>
+				</div>
+			</NavLink>
+			<NavLink to="/login">
 				<div className="option">
 					<i className="far fa-address-card"></i>
 					<h4>Log out</h4>
 				</div>
-			</a>
+			</NavLink>
 		</div>
 	</div>
     )

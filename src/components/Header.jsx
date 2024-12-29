e0d0c26f76976ce21c1446e6e23ed0467f4b0416
Sofía <sofiavillarejo.dev@gmail.com>
2024-12-29 20:40:54 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Component } from "react";
 import logo from "./../assets/images/logotipo-positivo.jpg";
+import services from '../services/services';
+import '../assets/css/header.css';
+class Header extends Component {
 
-const Header = () => {
-  return (
-    <div className='text-center'>
-      <img src={logo} alt="Logo" style={{width: "200px"}} />
-    </div>
-  );
+  state = {
+    perfilUsuario: []
+  }
+  componentDidMount() {
+    this.getUsuario();
+  }
+
+  getUsuario() {
+    services.getPerfilUsuario().then((response) => {
+      console.log(response);
+      this.setState({
+        perfilUsuario: response
+      })
+    })
+  }
+
+  render() {
+    const { perfilUsuario } = this.state;
+    return (
+      <div className="container container__header" >
+        <div className="header">
+          <div className='logo'>
+            <img src={logo} alt="Logo" style={{width: "200px"}} />
+          </div>
+        <div className="profile">
+          <div className="name_user">👋🏻 Hola, {perfilUsuario.usuario.nombre}</div>
+          <div className="user_photo">{perfilUsuario.usuario.imagen}</div>
+        </div>
+        </div>
+  
+      </div>
+    );
+  }
 };
 
 export default Header;

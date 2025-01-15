f86cfbeeb9c058f4d6c1e339b011e0d8a861a70f
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-15 12:36:51 +0100

    detalles Charla completo

--- src/components/Charlas.jsx
+++ src/components/Charlas.jsx
@@ -9,8 +9,8 @@ class Charlas extends Component {
 		rondas: [],
 		estadoCharla: [],
 		rondaSeleccionada: "",
-		seleccionadaCharla: null, 
-		showPopup: false, 
+		seleccionadaCharla: null,
+		showPopup: false,
 	}
 
 	getCharlas = () => {
@@ -54,7 +54,7 @@ class Charlas extends Component {
 			});
 		})
 	}
-	
+
 
 	handleCardClick = (charla) => {
 		this.setState({
@@ -71,72 +71,94 @@ class Charlas extends Component {
 	}
 
 	render() {
-    return (
-      <div className="container">
-        <h1 className="my-4 text-center">Charlas</h1>
-  
-        <div className="row d-flex flex-wrap justify-content-start">
-          {this.state.charlas.map((charla, index) => (
-            <div
-              key={index}
-              className="col-12 col-sm-6 col-md-4 mb-4"
-              onClick={() => this.handleCardClick(charla)}
-              style={{ cursor: "pointer" }}
-            >
-              <Card
-                imagen={charla.imagenCharla}
-                titulo={charla.titulo}
-                descripcion={charla.descripcion}
-              />
-            </div>
-          ))}
-        </div>
-  
-        <div className="row d-flex justify-content-end mt-4">
-          <div className="col-6 col-md-3">
-            <select
-              className="form-select"
-              value={this.state.rondaSeleccionada}
-              onChange={this.handleRondaChange}
-            >
-              <option value="">Ronda</option>
-              {this.state.rondas.map((ronda, index) => (
-                <option key={index} value={ronda.idRonda}>
-                  Ronda {ronda.idRonda}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="col-6 col-md-3">
-            <select className="form-select">
-              <option value="">Estado</option>
-              {this.state.estadoCharla.map((estado, index) => (
-                <option key={index} value={estado.idEstadoRonda}>
-                  {estado.estado}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <PopupCharla
-          show={this.state.showPopup}
-          onClose={this.handleClosePopup}
-        >
-          {this.state.seleccionadaCharla && (
-            <>
-              <h2>{this.state.seleccionadaCharla.titulo}</h2>
-              <img
-                src={this.state.seleccionadaCharla.imagenCharla}
-                alt={this.state.seleccionadaCharla.titulo}
-                className="img-fluid"
-              />
-              <p>{this.state.seleccionadaCharla.descripcion}</p>
-            </>
-          )}
-        </PopupCharla>
-      </div>
-    );
-  }
+		return (
+			<div className="container">
+				<div className="row d-flex justify-content-end mt-4">
+					<div className="col-6 col-md-3">
+						<select
+							className="form-select"
+							value={this.state.rondaSeleccionada}
+							onChange={this.handleRondaChange}
+						>
+							<option value="">Ronda</option>
+							{this.state.rondas.map((ronda, index) => {
+								return (
+									<option key={index} value={ronda.idRonda}>
+										Ronda {ronda.idRonda}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+					<div className="col-6 col-md-3">
+						<select
+							className="form-select"
+						>
+							<option value="">Estado</option>
+							{this.state.estadoCharla.map((estado, index) => {
+								return (
+									<option key={index} value={estado.idEstadoRonda}>
+										{estado.estado}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+				</div>
+
+				<h1 className="my-4 text-center">Charlas</h1>
+
+				{/* Fila de charlas */}
+				<div className="row d-flex flex-wrap justify-content-start">
+					{this.state.charlas.map((charla, index) => {
+						return (
+							<div key={index} className="col-12 col-sm-6 col-md-4 mb-4"
+								onClick={() => this.handleCardClick(charla)}
+								style={{ cursor: "pointer" }}>
+								<Card
+									imagen={charla.imagenCharla}
+									titulo={charla.titulo}
+									descripcion={charla.descripcion}
+								/>
+							</div>
+						);
+					})}
+				</div>
+				<PopupCharla
+					show={this.state.showPopup}
+					onClose={this.handleClosePopup}
+				>
+					{this.state.seleccionadaCharla && (
+						<>
+							<div className="charla_estado">
+								<span className="estado" style={{
+									backgroundColor: this.state.seleccionadaCharla.estadoCharla === 'ACEPTADA' ? '#b7eab0' : '#e5a879',
+									color: this.state.seleccionadaCharla.estadoCharla === 'ACEPTADA' ? '#29721f' : '#d57018',
+								}}>{this.state.seleccionadaCharla.estadoCharla}</span>
+							</div>
+							<div className="charla_title">
+								<div className="title">
+									<h2 className="poiret-one-regular">{this.state.seleccionadaCharla.titulo}</h2>
+									<hr className="card_divisor"></hr>
+								</div>
+								<div className="icon_tiempo">
+									<i class="fa-regular fa-clock"></i>
+									<span className="charla_tiempo">{this.state.seleccionadaCharla.tiempo} min.</span>
+								</div>
+							</div>
+							<div className="charla_image">
+								<img
+									src={this.state.seleccionadaCharla.imagenCharla}
+									alt={this.state.seleccionadaCharla.titulo}
+								/>
+								<span className="charla_descripcion">{this.state.seleccionadaCharla.descripcion}</span>
+							</div>
+						</>
+					)}
+				</PopupCharla>
+			</div>
+		);
+	}
 }
 
 

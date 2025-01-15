565b068a4d129ae930207aa19969887a6f44af34
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-15 16:57:21 +0100

    cambios

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -15,46 +15,46 @@ import UpdateProfileWrapper from './UpdateProfileWrapper';
 import LoginWrapper from './LoginWrapper';
 
 export default class Router extends Component {
-    render() {
+  render() {
 
-        function CharlasRonda() {
-            let {id} = useParams();
-            return  (<Charlas id={id} />)
-        }
-
-        return (
-            <BrowserRouter>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-12">
-                            <Header /> 
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-2 p-0">
-                            <Menu /> 
-                        </div>
-
-                        <div className="col-8">
-                                <Routes>
-                                    <Route path="/" element={<Home />} />
-                                    <Route path="/login" element={<LoginWrapper />} />
-                                    <Route path="/profile" element={<ProfileWrapper />} />
-                                    <Route path="/updateprofile" element={<UpdateProfileWrapper />} />
-                                    <Route path="/charlas" element={<Charlas />} />
-                                    <Route path="/charlas/:id" element={<CharlasRonda />} />
-									<Route path="/createronda" element={<CreateRonda />} />
-									<Route path="/createcharla" element={<CreateCharla />} />
-                                    <Route path="*" element={<NotFound />} />
-                                </Routes>
-                        </div>
-                        <div className="col-2">
-
-                        </div>
-                    </div>
-                </div>
-            </BrowserRouter>
-        );
+    function CharlasRonda() {
+      let { id } = useParams();
+      return (<Charlas id={id} />)
     }
+
+    return (
+      <BrowserRouter>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <Header />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-2 p-0">
+              <Menu />
+            </div>
+
+            <div className="col-8">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginWrapper />} />
+                <Route path="/profile" element={<ProfileWrapper />} />
+                <Route path="/updateprofile" element={<UpdateProfileWrapper />} />
+                <Route path="/charlas" element={<Charlas />} />
+                <Route path="/charlas/:id" element={<CharlasRonda />} />
+                <Route path="/createronda" element={<CreateRonda />} />
+                <Route path="/createcharla" element={<CreateCharla />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <div className="col-2">
+
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-//hola
+

4884b871d1fc0613b8a1b72893c754a013290d7f
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-20 10:11:49 +0100

    header con contenido

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { Component } from "react";
+import logo from "./../assets/images/logotipo-positivo.jpg";
+import services from '../services/services';
+import '../assets/css/header.css';
+import TooltipHeader from "./TooltipHeader";
+
+class Header extends Component {
+  state = {
+    tokenAvailable: false,
+    perfilUsuario: null,
+  };
+
+  componentDidMount() {
+    console.log("checktoken");
+    this.checkToken();
+  }
+
+
+  // Función para verificar si el token está presente
+  checkToken() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      this.setState({ tokenAvailable: true });
+      this.getUsuario();
+    } else {
+      this.setState({ tokenAvailable: false, perfilUsuario: null });
+    }
+  }
+
+  // Función para obtener el perfil del usuario
+  getUsuario() {
+    services.getPerfilUsuario().then((response) => {
+      this.setState({
+        perfilUsuario: response.usuario,
+      });
+    });
+  }
+
+  logout = () => {
+    services.logout();
+    this.setState({ tokenAvailable: false, perfilUsuario: null });
+    this.props.onLogout();
+  };
+
+  render() {
+    const { tokenAvailable, perfilUsuario } = this.state;
+    return (
+      <div className="container container__header">
+        <div className="header">
+          <div className="logo">
+            <img src={logo} alt="Logo" style={{ width: "200px" }} />
+          </div>
+          {/* Mostrar TooltipHeader solo si el token está presente */}
+          {tokenAvailable && perfilUsuario && <TooltipHeader perfilUsuario={perfilUsuario} logout={this.logout} />}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;

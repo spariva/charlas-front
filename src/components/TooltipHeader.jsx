b4789fc3be09a0f262c87bada30b2de500c57e3c
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-31 09:26:36 +0100

    tooltip profe crear ronda

--- src/components/TooltipHeader.jsx
+++ src/components/TooltipHeader.jsx
@@ -20,7 +20,7 @@ export default class TooltipHeader extends Component {
                 </svg>
               </span>
             </NavLink>
-            <NavLink to="/cursos">
+            <NavLink to="/createronda">
               <span className="tooltip2">
                 <svg xmlns="http://www.w3.org/2000/svg" height={20} width={20} viewBox="0 0 448 512">
                   <path fill="#ea8c4d" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
